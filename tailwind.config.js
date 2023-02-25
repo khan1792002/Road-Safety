19992e419da9cb00eb47506bcf97d5161bc33dff
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-25 16:41:15 +0530

    kutub(HomePage)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     './Pages/**/*.cshtml',
-    './Views/**/*.cshtml'
+    './Views/**/*.cshtml',
+    './Views/Home/*.cshtml',
+    './Views/Shared/*.cshtml'
   ],
   theme: {
     extend: {},

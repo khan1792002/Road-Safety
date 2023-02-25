d5bfa53776a0cd848b966fecc36cc2797da6f838
AnushreePan <pananushree708@gmail.com>
2023-02-25 18:17:12 +0530

    Anu ( Created Traffic Rules Pages )

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult TrafficRules()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
